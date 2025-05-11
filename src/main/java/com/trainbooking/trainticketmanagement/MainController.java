2db71c25e9cae22ff73456d19f90c8486601f979
cs-gaurav <gauravchuphal4444@gmail.com>
2025-05-12 00:41:39 +0530

    form-functionality

--- src/main/java/com/trainbooking/trainticketmanagement/MainController.java
+++ src/main/java/com/trainbooking/trainticketmanagement/MainController.java
@@ -3,6 +3,8 @@ import java.util.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import java.time.LocalDate;
+
 
 
 @Controller
@@ -11,35 +13,56 @@ public class MainController {
     public String homePage() {
         return "home";
     }
+
     @GetMapping("/book")
-    public String bookTicketPage() {
+    public String bookTicketPage(Model model) {
+        // Pass today's date to the Thymeleaf template
+        model.addAttribute("today", LocalDate.now());
         return "book";
     }
+
     @GetMapping("/show")
     public String pnrTicketPage() {
         return "show";
-    }  
+    }
+
     @GetMapping("/reschedule")
-    public String rescheduleTicketPage() {
+    public String rescheduleTicketPage(Model model) {
+        model.addAttribute("today", LocalDate.now());
         return "res";
-    }  
+    }
+
     @GetMapping("/upgrade")
     public String upgradeTicketPage() {
         return "upgrade";
     }
+
     @GetMapping("/cancel")
     public String cancelTicketPage() {
         return "cancel";
     }
 
-    @PostMapping("/showTicket")
-    public String handlePNRSubmission(@RequestParam Integer user_pnr, Model model) {
-        Map<String,Object> ticketDetails = UserFunctions.showTicket(user_pnr);
-        model.addAttribute("ticket", ticketDetails);
-        return "redirect:/ticket-show";
+    @PostMapping("/class-select")
+    public String bookTicket(@RequestParam("user_source") String source,
+                             @RequestParam("user_destination") String destination,
+                             @RequestParam("user_dot") String dot,
+                             Model model) {
+        model.addAttribute("source", source);
+        model.addAttribute("destination", destination);
+        model.addAttribute("dot", dot);
+        return "train-book";
     }
-    @GetMapping("/ticket-show")
-    public String showTicketInfo() {
+
+    @PostMapping("/user-details")
+    public String selectClass(@RequestParam("class") String trainClass, Model model) {
+        model.addAttribute("trainClass", trainClass);
+        return "input-details";  // Redirect to the seat layout page
+    }
+
+    @PostMapping("/submit-show")
+    public String handlePNRSubmission(@RequestParam Integer user_pnr, Model model) {
+        Map<String, Object> ticketDetails = UserFunctions.showTicket(user_pnr);
+        model.addAttribute("ticket", ticketDetails);
         return "ticket-show";
     }
 }
